3304400fbc197503cac6b1d47fde08cdf33206e7
hermben <hermben@gmail.com>
2021-12-11 23:16:44 -0800

    adds delete logic to users screen

--- src/Users.js
+++ src/Users.js
@@ -95,6 +95,26 @@ export class Users extends Component {
 
     }
 
+    deleteClick(id){
+        if(window.confirm('Are you sure?')){
+            fetch(variables.API_URL+'Users/'+id,{
+                method:'DELETE',
+                headers:{
+                    'Accept': 'application/json',
+                    'content-Type':'application/json'
+
+                }
+            })
+            .then(res=>res.json())
+            .then((result)=>{
+                alert(result);
+                this.refreshList();
+            },(error)=>{
+                alert('Failed');
+            })
+        }
+    }
+
 
 
     render() {
@@ -145,7 +165,8 @@ export class Users extends Component {
                                     </button>
 
                                     <button type="button"
-                                        className="btn btn-light mr-1">
+                                        className="btn btn-light mr-1"
+                                        onClick={()=>this.deleteClick(us.UserId)}>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
                                             <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
                                         </svg>
